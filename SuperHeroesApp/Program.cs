134d00c55cd05d6e8770f24cab0a473f82da83ed
Andres Neira <andresnei0820@gmail.com>
2026-03-10 12:57:44 -0500

    first

---
+++ SuperHeroesApp/Program.cs
@@ -0,0 +1,208 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Text;
+
+Console.WriteLine("Hello, World!");
+
+var fly = new SuperPower
+{
+    Name = "Flight",
+    Description = "The ability to fly through the air at will.",
+    Level = PowerLevel.Medium
+};
+
+var superStrength = new SuperPower
+{
+    Name = "Super Strength",
+    Description = "The ability to have strength far beyond that of a normal human.",
+    Level = PowerLevel.High
+};
+
+var iceBreath = new SuperPower
+{
+    Name = "Ice Breath",
+    Description = "The ability to exhale a freezing breath that can freeze objects and opponents.",
+    Level = PowerLevel.Medium
+};
+
+var laserEyes = new SuperPower
+{
+    Name = "Laser Eyes",
+    Description = "The ability to emit powerful laser beams from the eyes.",
+    Level = PowerLevel.High
+};
+
+var xRayVision = new SuperPower
+{
+    Name = "X-ray Vision",
+    Description = "The ability to see through solid objects.",
+    Level = PowerLevel.Low
+};
+
+var superman = new SuperHeroe();
+List<SuperPower> supermanPowers = new List<SuperPower>();
+supermanPowers.Add(fly);
+supermanPowers.Add(superStrength);
+supermanPowers.Add(iceBreath);
+supermanPowers.Add(laserEyes);
+supermanPowers.Add(xRayVision);
+
+superman.Name = "Superman";
+superman.Description = "A Kriptonian that lives on planet earth at metropolis, he loves to save the day and defeat Lex Luthor";
+superman.SecretIdentity = "Clark Kent";
+superman.City = "Metropolis";
+superman.HasSuperPowers = true;
+superman.SuperPowers = supermanPowers;
+superman.IsAlive = true;
+superman.IsHuman = false;
+superman.CanFly = true;
+// superman.UseSuperPower();
+string resultPowers = superman.UseSuperPower();
+Console.WriteLine(resultPowers);
+
+var batman = new SuperHeroe();
+
+batman.Name = "Batman";
+batman.Description = "A man with a bat suit that defends Gotham city from villians like Joker, Penguin and the Riddler";
+batman.SecretIdentity = "Bruce Wayne";
+batman.City = "Gotham";
+batman.HasSuperPowers = false;
+batman.IsAlive = true;
+batman.IsHuman = true;
+batman.CanFly= false;
+
+
+
+class SuperHeroe
+{
+    private List<SuperPower> _superPowers;
+    private bool _hasSuperPowers;
+
+    public Guid Id { get; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public string SecretIdentity { get; set; }
+    public string City { get; set; }
+    public bool HasSuperPowers
+    {
+        get => _hasSuperPowers;
+        set
+        {
+            _hasSuperPowers = value;
+            if (!value)
+            {
+                _superPowers = null;
+            }
+        }
+    }
+    public List<SuperPower> SuperPowers
+    {
+        get => _superPowers;
+        set
+        {
+            if (!_hasSuperPowers)
+            {
+                throw new InvalidOperationException("Cannot set SuperPowers when HasSuperPowers is false.");
+            }
+            _superPowers = value;
+        }
+    }
+
+    public bool IsAlive { get; set; }
+    public bool IsHuman { get; set; }
+    public bool CanFly { get; set; }
+
+    /* This DOES NOT RETURN ANYTHING, it just prints the powers to the console, but we want to return a string with the powers
+    public void UseSuperPower()
+    {
+        
+        foreach (var power in SuperPowers)
+        {
+            Console.WriteLine($"{Name} uses {power.Name}!");
+        }
+
+    }
+    */
+
+    //Method that returns a string with the powers of the superhero
+    public string UseSuperPower()
+    {
+        StringBuilder sb = new StringBuilder();
+        foreach (var power in SuperPowers)
+        {
+            sb.AppendLine($"{Name} uses {power.Name}!!!");
+        }
+
+        return sb.ToString();
+    }
+
+    //Constructor
+    public SuperHeroe()
+    {
+        Id = Guid.NewGuid();
+        Name = "";
+        Description = "";
+        SecretIdentity = "";
+        City = "";
+        _hasSuperPowers = false;
+        _superPowers = new List<SuperPower>();
+        IsAlive = true;
+        IsHuman = true;
+        CanFly = false;
+    }
+}
+
+public class SuperPower
+{
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public PowerLevel Level { get; set; }
+
+    //Constructor
+    public SuperPower()
+    {
+        Name = "";
+        Description = "";
+        Level = PowerLevel.Low;
+
+    }
+
+}
+
+public enum PowerLevel
+{
+       Low,
+    Medium,
+    High,
+    Godlike
+}
+
+//Here is a RECORD instead of a class, it is immutable and has a built-in constructor, it is used to represent data that does not change, like a DTO (Data Transfer Object) or a Value Object
+public record SuperHeroRecord(
+    Guid Id, 
+    string Name, 
+    string Description, 
+    string SecretIdentity, 
+    string City, 
+    bool HasSuperPowers, 
+    IReadOnlyList<SuperPower> SuperPowers, 
+    bool IsAlive, 
+    bool isHuman, 
+    bool Canfly)
+{
+    public string UseSuperPower()
+    {
+        if (!HasSuperPowers || SuperPowers is null || SuperPowers.Count == 0)
+        {
+            return $"{Name} has no super powers.";
+        }
+
+        StringBuilder sb = new StringBuilder();
+
+        foreach (var power in SuperPowers)
+        {
+            sb.AppendLine($"{Name} uses {power.Name}!!!");
+        }
+
+        return sb.ToString();
+    }
+} // Example of using the RECORD
